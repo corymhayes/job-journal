a90854b7cc990f87d271a39851c846563a9fab56
Cory Hayes <corymhayes@gmail.com>
2026-03-18 09:18:51 -0500

    - testing binding

--- src/worker/db/index.ts
+++ src/worker/db/index.ts
@@ -1,3 +1,9 @@
 import { drizzle } from "drizzle-orm/neon-http";
 
 export const db = drizzle(process.env.DATABASE_URL!);
+
+export default {
+  async fetch(request, env) {
+    console.log(await env.DB_URL.get());
+  },
+};
